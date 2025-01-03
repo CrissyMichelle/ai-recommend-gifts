f74974f38b1a82f10100693094790c3d92c48065
Crissy Cabrera <crissymichelle@proton.me>
2025-01-03 10:44:41 -0500

    updated app.py to run in prod mode

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def refresh():
     return render_template("index.html", form=GiftForm(), gifts=None)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5001)
